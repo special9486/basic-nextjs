1fab8a3601290d4415f0aa961bb3c2ab8f5a9ecf
SeongHeon <special9486@finger.co.kr>
2023-10-27 17:58:28 +0900

    셈플 코드에 High Order Function 적용

--- components/com/CustomLink.js
+++ components/com/CustomLink.js
@@ -6,10 +6,10 @@ const { HOF } = ComponentInitializer.init('CustomLink');
 export default HOF(({children, href, className}) => {
     const core = useCore();
 
-    const clickEvent = (event) => {
+    const clickEvent = HOF((event) => {
         event.preventDefault();
         core.goPage(href);
-    };
+    }, 'clickEvent');
 
     return (
         <a href={'#'} className={className} onClick={clickEvent}>{children}</a>

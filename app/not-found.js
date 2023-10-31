7cccdc237da930628ea333159658d6354cfdf4b7
SeongHeon <special9486@finger.co.kr>
2023-10-31 13:25:14 +0900

    404 커스텀 페이지 추가

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div>
+            <h1>Page Not Found.....</h1>
+            <p>커스터마이징 경로: /app/not-found.js</p>
+        </div>
+    )
+};

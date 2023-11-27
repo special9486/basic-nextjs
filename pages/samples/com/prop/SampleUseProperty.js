aef1583522eeff8ad1622023069c21458d245aad
SeongHeon <special9486@finger.co.kr>
2023-11-27 17:51:23 +0900

    환경별 프로퍼티 처리 구조 적용 => 오타 수정

--- pages/samples/com/prop/SampleUseProperty.js
+++ pages/samples/com/prop/SampleUseProperty.js
@@ -10,13 +10,13 @@ export default HOF(() => {
         <Layout>
             <h1>프로퍼티 사용 셈플</h1>
             <p>프로퍼티는 config 폴더 하위에 각 환경별로 존재하는 property 파일에 정의한다.</p>
-            <ui>
+            <ul>
                 <li>defaultProperty.js: 공통 프로퍼티</li>
                 <li>localProperty.js: local PC 환경 프로퍼티</li>
                 <li>devProperty.js: 개발 서버 환경 프로퍼티</li>
                 <li>stgProperty.js: 스테이징 서버 환경 프로퍼티</li>
                 <li>prodProperty.js: 운영 환경 프로퍼티</li>
-            </ui>
+            </ul>
             <p>defaultProperty.js 파일은 모든 환경에서 사용되는 프로퍼티를 정의한다.</p>
             <p>defaultProperty.js 와 중복된 환경 프로퍼티 항목은 덮어씌워진다.</p>
             <p>프로퍼티에 정의된 env 값: { PropertyUtils.getProp('env') }</p>
